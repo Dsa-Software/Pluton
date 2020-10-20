1a2e5d3278cd5d7aa15b6ab22d2cd166ec43130a
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-10-20 14:13:56 -0300

    Delete save.ts

--- app/ts/save.ts
+++
@@ -1,6 +0,0 @@
-function save() {
-    let texto = document.getElementById("code").value;
-    let titulo = document.getElementById("in").value;
-    let blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, titulo);
-}
