4b6324dc5ea8207a48a21a5e5604bd033e72df5c
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-10-21 08:06:26 -0300

    Name alterations

--- app/ts/file-tab.ts
+++ app/ts/file-tab.ts
@@ -1,4 +1,10 @@
 function tab_name() {
     let input = document.getElementById("in").value
+    if (input == "") {
+        input="file.txt"
+    }
+    if(input.length >= 7) {
+        input = input.slice(0, 7) + "..."
+    }
     document.getElementById("file-name").textContent = input
 }
